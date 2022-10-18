c68cea5d8e26df002a2e43f93a13f77265410d9d
norbertodiaz74 <norberto.diaz74@gmail.com>
2022-10-18 11:59:11 -0300

    eje 3

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -29,6 +29,12 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  if (status === 1){
+    return "Online";
+  } else if (status === 2) {
+    return "Away";
+  }
+  return "Offline";
 }
 
 function saludo(idioma) {
