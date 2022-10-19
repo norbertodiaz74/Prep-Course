8936e3ff90e15d7acb7f3b3d6df5c992af4d1ed1
norbertodiaz74 <norberto.diaz74@gmail.com>
2022-10-19 13:30:51 -0300

    5

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -66,8 +66,6 @@ function dePalabrasAFrase(palabras) {
       return MyPalabras;
     }
     MyPalabras = MyPalabras + " " + palabras[i+1];
-    
-
   }
   
 }
@@ -89,6 +87,11 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+var Mysuma = 0;
+for (var i =0; i < numeros.length; i++){
+Mysuma = Mysuma + numeros[i];
+}
+return Mysuma;
 }
 
 
@@ -96,6 +99,13 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  var Mysuma = 0;
+  var MyPromedio =0;
+for (var i =0; i < numeros.length; i++){
+Mysuma = Mysuma + numeros[i];
+}
+MyPromedio = Mysuma / numeros.length
+return MyPromedio;
 }
 
 
@@ -103,6 +113,17 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  var MyMayor = 0;
+
+for (var i =0; i < numeros.length; i++){
+  if (numeros[i] > MyMayor){
+    MyMayor = numeros[i];
+  }
+
+}
+
+return  MyMayor;
+
 }
 
 
@@ -110,13 +131,33 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  var MyProducto =1;
+  if (arguments.length === 0){
+    return 0;
+  }
+  if (arguments.length === 1){
+    return arguments;
+  }
+  for (var i =0; i < arguments.length; i++){
+    MyProducto = MyProducto * arguments[i];
+  }
+  return MyProducto;
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
+  var MyMayor18 = 0;
 
+for (var i =0; i < numeros.length; i++){
+  if (numeros[i] > 18){
+    MyMayor18 = MyMayor18 + 1;
+  }
+
+}
+
+return   MyMayor18;
 }
 
 
@@ -125,7 +166,10 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  if ((numeroDeDia ===1) || (numeroDeDia ===7)){
+    return "Es fin de semana";
+  }
+    return "Es dia Laboral";
 } 
 
 
@@ -133,7 +177,12 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  var MyNroText = toString(n);
+  if (MyNroText[0] === "9"){
+    return true;
+  }
+  return false;
+
 }
 
 
@@ -141,7 +190,17 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
+var MyElemento = arreglo[0];
+
+for (var i =1; i < arreglo.length; i++){
+
+  if !(arreglo[i] === MyElemento){
+    return false;
+  }
   
+}
+
+return   true;
 } 
 
 
